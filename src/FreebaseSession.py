b1087a920b01ba68cb632dfefaa9d86d3476ddd6
tfmorris <tfmorris@e5da0940-7bb3-11de-bb5b-0fd25fbaf517>
2010-02-03 05:47:48 +0000

    Refactor for better encapsulation

--- src/FreebaseSession.py
+++ src/FreebaseSession.py
@@ -6,12 +6,13 @@ Created on Aug 8, 2009
 @copyright: 2009 Thomas F. Morris
 '''
 
+import json
 import logging
 from optparse import OptionParser
-import getpass
 
 from freebase.api import HTTPMetawebSession, MetawebError
 
+SEPARATORS = (",", ":")
 
 def getSessionFromArgs():
     '''Convenience method to create a Freebase session using the username, 
@@ -20,7 +21,7 @@ def getSessionFromArgs():
     
     log = logging.getLogger('FreebaseSession')
     parser = OptionParser()
-    parser.add_option("-u", "--user", dest="user", help="Freebase username", default='tfmorris')
+    parser.add_option("-u", "--user", dest="user", help="Freebase username")
     parser.add_option("-p", "--password", dest="pw", help="Freebase password")
     parser.add_option("-s", "--host", dest="host", help="service host", default = 'www.sandbox-freebase.com')
    
@@ -35,7 +36,7 @@ def getSessionFromArgs():
 #        pw = getpass.getpass()
 
     log.info( 'Host: %s, User: %s' % (host, user))
-    return HTTPMetawebSession(host, username=user, password=pw)
+    return FreebaseSession(host, username=user, password=pw)
 
 class FreebaseSession(HTTPMetawebSession):
     '''
@@ -93,6 +94,119 @@ class FreebaseSession(HTTPMetawebSession):
             return []
 #       log.debug([ '    Response = ',response])    
         return response
+    
+    def fbQueryName(self,name,subquery):
+        '''Query Freebase for a topic with the given name (including aliases)'''
+        query = {'/type/reflect/any_value' : [{'lang' : '/lang/en',
+                                            'link|=' : ['/type/object/name',
+                                                        '/common/topic/alias'],
+                                            'type' : '/type/text',
+                                            'value' : name}],
+                'name' : None, # return actual formal name of topic
+                'id' : None,
+                'guid' : None
+        }
+        query.update(subquery)
+        return self.fbRead([query])
+    
+    
+    def queryTypeAndName(self, type, names, createMissing = False):
+        '''Query server for given type(s) by name(s) in name or alias field.  
+        Return list of GUIDs which match single type.
+        If "type" parameter is a list, look at all types and return list of tuples containing
+        (guid,[list of matching types]).  createMissing may not be used for multiple types.
+        Return empty list if none of the names are found.  Ambiguous matches are not returned.'''
+            
+        if not names:
+            return []
+    
+        if isinstance(type,list):
+            typeq={'type|=':type,'type':[]}
+        else:
+            typeq={'type':type}
+        
+        ids = []
+        for name in names:
+            results = self.fbQueryName(name,typeq)
+            if not results:
+                self.log.debug(' '.join(['Warning: name not found', name, 'type:', repr(type)]))
+                if createMissing:
+                    if isinstance(type,str):
+                        guid = self.createTopic(name, [type])
+                        if guid:
+                            ids.append(guid)
+                            self.log.info('Created new topic ' + str(guid) + '  ' + name)
+                        else:
+                            self.log.error('Failed to create new entry ' + name + ' type: ' + type)
+                    else:
+                        self.log.error('Cannot create topic when searching for multiple types')
+            elif len(results) == 1:
+                guid = results[0]['guid']
+                if isinstance(type,str):
+                    ids.append(guid)
+                else:
+                    ids.append((guid,results[0]['type']))
+    #            log.debug(['                                                          found ', name])
+            else:
+                self.log.warn('Non-unique name found for unique lookup ' + name +' type: ' + repr(type)) 
+                # TODO We could create a new entry here to be manually disambiguated later
+    #            if createMissing:
+    #                guid = self.createTopic(name, type)
+    #                if guid:
+    #                    ids.append(guid)
+    #                    log.info('Created new topic which may need manual disambiguation ', guid, '  ', name)
+    #                else:
+    #                    log.error('Failed to create new entry ', name, ' type: ', type)
+        return ids
+
+           
+    def createTopic(self, name, types):
+        common = "/common/topic"
+        if not common in types: # make sure it's a topic too
+            types.append(common)
+        query = {'create': 'unconditional', # make sure you've checked to be sure it's not a duplicate
+                 'type': types,
+                 'name' : name,
+                 'guid' : None
+                 }
+        response = self.fbWrite(query)
+        guid = response['guid']
+        return guid
+
+#    def freeqSubmit(self, triples, graphport='sandbox', comment=None,
+#                    tool_id='/guid/9202a8c04000641f800000001378d774'):
+#        """do a mql write. For a more complete description,
+#        see http://www.freebase.com/view/en/api_service_mqlwrite"""
+#
+#        query = {}
+#        query['user']=''
+#        query['action_type']='LOAD_TRIPLE'
+#        query['operator']='/user/spreadsheet_bot'
+#        query['check_params']=False
+#        query['graphport']=graphport
+#        query['mdo_info']={"software_tool":tool_id,"name":comment}
+#        query['payload']=triples
+#        
+#        qstr = json.dumps(query, separators=SEPARATORS)
+#        
+#        self.log.debug('FREEQSUBMIT: %s', qstr)
+#        
+#        service = '/freeq/spreadsheet'
+#        
+#        self.log.info('%s: %s',
+#                      service,
+#                      query)
+#
+#        r = self._httpreq_json(service, 'GET')
+#        
+#        headers = {'Referer' : 'http://data.labs.freebase.com/loader/',
+#                   'Origin' : 'http://data.labs.freebase.com'}
+#        
+#        r = self._httpreq_json(service, 'POST',
+#                               form=query, headers=headers)
+#        
+#        self.log.debug('FREEQSUBMIT RESP: %r', r)
+#        return self._mqlresult(r)
     
 if __name__ == "__main__":
     session = FreebaseSession('www.sandbox-freebase.com','tfmorris','password')
