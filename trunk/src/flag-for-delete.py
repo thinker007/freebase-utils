92882f8d7eaae40bd6a8164145a821d2a911dd45
tfmorris <tfmorris@e5da0940-7bb3-11de-bb5b-0fd25fbaf517>
2010-09-15 19:32:18 +0000

    Update to write new style merge flags.

--- trunk/src/flag-for-delete.py
+++ trunk/src/flag-for-delete.py
@@ -21,9 +21,9 @@ from freebase.api import HTTPMetawebSession
 
 def main():
     parser = OptionParser()
-    parser.add_option("-u", "--user", dest="user", help="Freebase username", default='tfmorris')
-    parser.add_option("-p", "--password", dest="pw", help="Freebase password")
-    parser.add_option("-s", "--host", dest="host", help="service host", default = 'api.sandbox-freebase.com')
+    parser.add_option('-u', '--user', dest='user', help='Freebase username', default='tfmorris')
+    parser.add_option('-p', '--password', dest='pw', help='Freebase password')
+    parser.add_option('-s', '--host', dest='host', help='service host', default = 'api.sandbox-freebase.com')
    
     (options, args) = parser.parse_args()
 
@@ -38,44 +38,51 @@ def main():
     session = HTTPMetawebSession(host, username=user, password=pw)
     session.login()
     
-    # Topics which have been typed as Wikipedia disambiguation pages (from BFG WEX analysis)
-    # and which have no other types assigned and which are not flagged for delete already
-    q = {# Only typed with /common/topic and our disambig page type
-         't1:type': [{
-            'i1:id!=':  '/user/tfmorris/default_domain/wikipedia_disambiguation_page',
-            'i2:id!=':  '/common/topic',
-            'id':       None,
-            'name':     None,
-            'optional': 'forbidden',
-            'type':     '/type/type'
+    # Query which matches topics to be deleted    
+    q = { '!pd:/base/jewlib/judaica_owner/research_collections': [{
+            'id': '/m/0cbl9hh'
           }],
-          'id':      None,
-          'name':    None,
-          'type':    [],
-          't2:type': '/user/tfmorris/default_domain/wikipedia_disambiguation_page',
-          # Not flagged for delete
-          '!/freebase/opinion_collection/mark_for_delete': {
-            'id':       None,
-            'optional': 'forbidden'
-          }
+          'timestamp': [{
+            'type':    '/type/datetime',
+            'value<':  '2010-09-10',
+            'value>=': '2010-09-09'
+          }],
+          'creator': {
+            'id': '/user/frankschloeffel'
+          },
+          'id':            None,
+          'name':          None,
+          'sort':          'name',
+          'ns0:timestamp': None,
+          'type':          '/base/jewlib/research_collection',
+          # Not already flagged for delete
+          '!/freebase/review_flag/item':{'id':None,'optional':'forbidden'}
         }
     
+    # Old style delete flagWrite query to add topic to the flagged for delete collection
+#    wq = {'type' : '/freebase/opinion_collection',
+#          'authority' : '/user/'+user,
+#          'mark_for_delete': {
+#            'id' : None,
+#            'connect' : 'insert'
+#          }
+#      }
+    
     # Write query to add topic to the flagged for delete collection
-    wq = {'type' : '/freebase/opinion_collection',
-          'authority' : '/user/'+user,
-          'mark_for_delete': {
-            'id' : None,
-            'connect' : 'insert'
-          }
-      }
+    wq =  {'type': '/freebase/review_flag',
+           'kind': {'id': '/freebase/flag_kind/delete'},
+           'item': {'id': None},
+           'create': 'unless_exists'
+         }
       
     result = session.mqlreaditer([q])
     
     count = 0
     for r in result:
         count += 1
-        print '\t'.join([str(count),r.id,','.join(r.type)])
-        wq['mark_for_delete']['id'] = r.id
+        name = r.name if r.name else '*null*'
+        print '\t'.join([str(count),r.id,name])
+        wq['item']['id'] = r.id
         session.mqlwrite(wq)
     
     print 'Marked a total of %d topics for delete on %s' % (count, host)
